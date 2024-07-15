4561597cb778d86f299a97c02a780309e4f51084
harry <harry@alphashed.cn>
2024-07-15 11:35:08 +0800

    24/07/15 11_1_1

---
+++ 11_1_1/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <cstdio>
+int n;
+int train[10000];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", train + i);
+    }
+    int ans = 0;
+    int cnt = 1;
+    int i = 1;
+    while (cnt != 0)
+    {
+         cnt = 0;
+        for (int j = 0; j < n - i; j++)
+        {
+            if (train[j] > train[j + 1])
+            {
+                int tmp = train[j];
+                train[j] = train[j + 1];
+                train[j + 1] = tmp;
+                cnt++;
+            }
+        }
+        i++;
+        ans += cnt;
+    }
+    printf("%d", ans);
+    return 0;
+}
--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -26,7 +26,7 @@ int main()
         }
         i++;
     }
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
+    for (int j = 0; j < n; j++)
+        printf("%d ", a[j]);
     return 0;
 }
