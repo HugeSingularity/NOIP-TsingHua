dc1387dedd32a892e4d9c94a01afb9e9c312dba2
harry <harry@alphashed.cn>
2024-07-15 14:38:30 +0800

    24/07/15 11_2_1

---
+++ 11_2_1/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+
+int n, m;
+int mark[1000];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    int ans = 1;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", mark + i);
+        if (mark[i] > m)
+            ans++;
+    }
+    printf("%d", ans);
+    return 0;
+}
