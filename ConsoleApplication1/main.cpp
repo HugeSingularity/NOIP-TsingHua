ca758f8f5de5e512b8a58039e7b2c63003386ec9
harry <harry@alphashed.cn>
2024-07-15 11:17:54 +0800

    添加项目文件。

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <cstdio>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", a + i);
+    }
+    int cnt;
+    int i = 1;
+    while (cnt != 0)
+    {
+        cnt = 0;
+        for (int j = 0; j < n - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+                cnt++;
+            }
+        }
+        i++;
+    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
