be11922c6e9c1629a5f3a5a211ff6e1c8d07c261
harry <harry@alphashed.cn>
2024-07-17 16:38:23 +0800

    Date : 24/07/17

---
+++ 12_1/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", a + i);
+    }
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == m) printf("%d", i);
+    }
+    return 0;
+}
---
+++ 12_2/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int find(int a[], int l, int r, int m)
+{
+    int middle = (r - l) / 2;
+    if (a[middle] < m) find(a, middle + 1, r, m);
+    else if (a[middle] > m) find(a, l, middle - 1, m);
+    else return middle;
+}
+
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", a + i);
+    }
+    sort(a, a + n);
+    find(a, 0, n - 1, m);
+    return 0;
+}
