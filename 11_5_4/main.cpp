c2b275d82440d83a507f6664c9514f08ad914fda
harry <harry@alphashed.cn>
2024-07-17 10:27:17 +0800

    Date:24/07/17

---
+++ 11_5_4/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int number[n];
+    for (int i = 0; i < n; i++)
+    scanf("%d", number + i);
+    sort(number, number + n);
+    for (int i = 1, j = n - 1, k = 0; i <= n; i++)
+    {
+        if (i % 2 == 1) printf("%d\n", number[j--]);
+        else printf("%d\n", number[k++]);
+    }
+    return 0;
+}
