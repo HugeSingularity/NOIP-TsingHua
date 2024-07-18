791111beb9444c30e7f1c86193048c98dd493520
harry <harry@alphashed.cn>
2024-07-18 10:02:43 +0800

    Date:24/07/18

---
+++ 12_3_1/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int a[1000000];
+
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    memset(a, -1, sizeof(a));
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        scanf("%d", &j);
+        a[j] = i;
+    }
+    int ans[m];
+    for (int i = 0; i < m; i++)
+    {
+        int j;
+        scanf("%d", &j);
+        ans[i] = a[j];
+    }
+    for (int i = 0; i < m; i++)
+        printf("%d\n", ans[i]);
+    return 0;
+}
