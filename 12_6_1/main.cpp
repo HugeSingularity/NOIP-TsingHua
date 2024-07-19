aaf6f661eab4f3caede407911e984589f1159cf2
harry <harry@alphashed.cn>
2024-07-19 16:43:09 +0800

    24/07/19

---
+++ 12_6_1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdio>
+
+bool check(int x)
+{
+
+}
+
+int Search(int a[], int l, int r, int x)
+{
+	while (l < r)
+	{
+		int mid = l + r >> 1;
+		if (check(x))
+		{
+			r = mid;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+		return l;
+	}
+}
+
+int main()
+{
+    return 0;
+}
---
+++ 12_6_2/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdio>
+
+bool check(int x)
+{
+
+}
+
+int Search(int a[], int l, int r, int x)
+{
+	while (l < r)
+	{
+		int mid = l + r + 1 >> 1;
+		if (check(x))
+		{
+			l = mid;
+		}
+		else
+		{
+			r = mid + 1;
+		}
+		return l;
+	}
+}
+
+int main()
+{
+    return 0;
+}
