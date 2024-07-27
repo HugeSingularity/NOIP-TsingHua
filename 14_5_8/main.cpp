91e44133088e7ce0b002a2785c2b8fb079235ec5
harry <harry@alphashed.cn>
2024-07-27 15:28:19 +0800

    24/07/25

---
+++ 14_5_8/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <cstdio>
+
+int N;
+double D1, C, D2, P;
+
+int main()
+{
+    scanf("%u%u%u%u%d", &D1, &C, &D2, &P, &N);
+    double distance[N], price[N];
+    scanf("%u%u", distance, price);
+    int min = 0;
+    for (int i = 1; i < N; i++)
+    {
+        int tmp;
+        scanf("%u%u", &tmp, price + i);
+        tmp -= distance[i - 1];
+        distance[i] = tmp;
+        if (distance[i] > C * D2)
+        {
+            printf("No Solution");
+            return 0;
+        }
+        if (price[i] < price[min]) min = i;
+    }
+
+    return 0;
+}
